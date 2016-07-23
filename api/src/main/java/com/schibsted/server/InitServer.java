45dd33c49e1d5f3c7bf782879ebb76f71a773711
admin <admin@samuris.com>
2016-07-23 10:17:42 +0200

    1.-Added basic maven infraestructure. 2.-Added server and main class

---
+++ api/src/main/java/com/schibsted/server/InitServer.java
@@ -0,0 +1,19 @@
+package com.schibsted.server;
+
+import com.schibsted.server.handler.InitHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * Created by edu on 23/07/2016.
+ */
+public class InitServer {
+
+    public static void startServer() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(15000), 0);
+        server.createContext("/", new InitHandler());
+        server.start();
+    }
+}
---
+++ api/src/main/java/com/schibsted/server/handler/InitHandler.java
@@ -0,0 +1,15 @@
+package com.schibsted.server.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+/**
+ * Created by edu on 23/07/2016.
+ */
+public class InitHandler implements HttpHandler {
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
+}
